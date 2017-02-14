a7aa3a663c9dcbab07b73da562f98fe5ebdb4663
professionalcodes <dylan.dannenhauer@protonmail.com>
2017-02-13 21:00:46 -0600

    adding test functions to codingbat problems

--- codingbat-in-bash.sh
+++ codingbat-in-bash.sh
@@ -3,7 +3,6 @@
 # The parameter weekday is true if it is a weekday, and the parameter 
 # vacation is true if we are on vacation. We sleep in if it is not a 
 # weekday or we're on vacation. Return true if we sleep in.
-
 # sleepIn(false, false) → true
 # sleepIn(true, false) → false
 # sleepIn(false, true) → true
@@ -12,10 +11,17 @@ function sleepIn {
 	vacation_bool=$2
 	
 	if [ $weekday_bool -eq "false" -o $vacation_bool -eq "true" ]; then
-		return 0
+		echo "true"
 	else 
-		return 1
+		echo "false"
 	fi
+}
+
+function test_sleepIn {
+	sleepIn false false 
+	sleepIn true false
+	sleepIn false true 
+	echo "true false true"
 }
 
 # We have two monkeys, a and b, and the parameters 
@@ -24,47 +30,55 @@ function sleepIn {
 # monkeyTrouble(true, true) → true
 # monkeyTrouble(false, false) → true
 # monkeyTrouble(true, false) → false
-
 function monkeyTrouble {
 	monkeyA=$1
 	monkeyB=$2
 
 	if [ monkeyA = "true" ] && [ monkeyB = "true" ]; then
-		return 0
+		echo "true"
 	elif [ monkeyA = "false" ] && [ monkeyB = "false" ]; then
-		return 0
+		echo "true"
 	else
-		return 1
+		echo "false"
 	fi
+}
+
+function test_monkeyTrouble {
+	monkeyTrouble true true 
+	monkeyTrouble false false
+	monkeyTrouble true false
+	echo "true true false"
 }
 
 # Given two int values, return their sum. 
 # Unless the two values are the same, 
 # then return double their sum.
-
 # sumDouble(1, 2) → 3
 # sumDouble(3, 2) → 5
-# sumDouble(2, 2) → 9
-
+# sumDouble(2, 2) → 8
 function sumDouble {
 	num1=$1
 	num2=$2
 
 	if [ $num1 -eq $num2 ]; then
-		echo "($num1 + $num2) * 2" | bc
+		(( doubleSum=($num1+$num2)*2 )) && echo $doubleSum
 	else
-		echo "($num1 + $num2)" | bc
+		(( sum=$num1+$num2 )) && echo $sum
 	fi
 }
 
+function test_sumDouble {
+	sumDouble 1 2
+	sumDouble 3 2
+	sumDouble 2 2
+	echo "3 5 8"
+}
 
 # Given an int n, return the absolute difference between n and 21, 
 # except return double the absolute difference if n is over 21.
-
 # diff21(19) → 2
 # diff21(10) → 11
 # diff21(21) → 0
-
 function diff21 {
 	num1=$1
 	if [ $num1 -gt 21 ]; then
@@ -77,8 +91,14 @@ function diff21 {
 	echo $abs_difference
 }
 
-# We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+function test_diff21 {
+	diff21 19
+	diff21 10
+	diff21 21
+	echo "2 11 0"
+}
 
+# We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
 # parrotTrouble(true, 6) → true
 # parrotTrouble(true, 7) → false
 # parrotTrouble(false, 6) → false
@@ -93,6 +113,18 @@ function parrotTrouble {
 	fi
 }
 
+function test_parrotTrouble {
+	parrotTrouble true 6
+	parrotTrouble true 7
+	parrotTrouble false 6
+	echo "true false false"
+}
+
+
+# Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+# makes10(9, 10) → true
+# makes10(9, 9) → false
+# makes10(1, 9) → true
 function makes10 {
 	if [ $1 -eq 10 -o $2 -eq 10 ]; then
 		return 0
@@ -106,13 +138,20 @@ function makes10 {
 	fi
 }
 
+function test_makes10 {
+	makes10 9 10
+	makes10 9 9
+	makes10 1 9
+	echo "true false true"
+}
+
 
 # Given an int n, return true if it is within 10 of 100 or 200. 
-# Note: Math.abs(num) computes the absolute value of a number in java, but this is bash so ur out of luck.
+# Note: Math.abs(num) computes the absolute value of a number in java, 
+# but this is bash so ur out of luck.
 # nearHundred(93) → true
 # nearHundred(90) → true
 # nearHundred(89) → false
-
 function nearHundred {
 	if [ $1 -ge 90 ] && [ $1 -le 110 ]; then 
 		return 0
@@ -123,14 +162,19 @@ function nearHundred {
 	fi
 }
 
+function test_nearHundred {
+	nearHundred 93
+	nearHundred 90
+	nearHundred 89
+	echo "true true false"
+}
+
 # Given 2 int values, return true if one is negative and one is positive. 
 # Except if the parameter "negative" is true, 
 # then return true only if both are negative.
-
 # posNeg(1, -1, false) → true
 # posNeg(-1, 1, false) → true
 # posNeg(-4, -5, true) → true
-
 function posNeg {
 	num1=$1
 	num2=$2
@@ -147,8 +191,68 @@ function posNeg {
 	fi
 }
 
+function test_posNeg {
+	posNeg 1 -1 false
+	posNeg -1 1 false
+	posNeg -4 -4 true 
+	echo "true true true"
+}
+
+
+# Given a string, return a new string where "not " 
+# has been added to the front. However, if the string already 
+# begins with "not", return the string unchanged. 
+# notString("candy") → "not candy"
+# notString("x") → "not x"
+# notString("not bad") → "not bad"
+function notString {
+	string=$1
+	first_three_letters=${string:0:3}
+	if [ $first_three_letters = "not" ]; then 
+		echo $string 
+	else
+		string="not $string"
+		echo $string 
+	fi
+}
+
+function test_notString {
+	notString "candy"
+	notString "x"
+	notString "not bad"
+	echo "not candy, not x, not bad"
+}
+
+
 function test_it {
-	
+	echo "testing notString"
+	test_notString
+
+	echo "testing posNeg"
+	test_posNeg
+
+	echo "testing nearHundred"
+	test_nearHundred
+
+	echo "testing makes10"
+	test_makes10
+
+	echo "testing parrotTrouble"
+	test_parrotTrouble
+
+	echo "testing diff21"
+	test_diff21
+
+	echo "testing sumDouble"
+	test_sumDouble
+
+	echo "testing monkeyTrouble"
+	test_monkeyTrouble
+
+	echo "testing sleepIn"
+	test_sleepIn
 }
 
 test_it
+
+
